4d634db2a3a37d801661ddcacf45d7ddfaa60c69
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-08 21:31:57 +0930

    add functional timer with penalty function

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ var openingDiv = document.getElementById('opener');
 var answerbtn = document.getElementsByClassName("answer");
 var navBar = document.getElementById('navbar');
 var myCarousel = document.getElementById('mycarousel');
+var timerDisplay = document.getElementById('timer-display');
+var dots = document.getElementById('digit-dots');
+var addFive = document.getElementById('addfive');
+var enterNameDiv = document.getElementById('enter-name');
+var yourScore = document.getElementById('your-score');
+var name = document.getElementById('name');
+var scoreBoard = document.getElementById('scoreboard');
+
+var timeoutColor;
 var timer;
 var seconds = 0;
 var minutes = 0;
@@ -10,6 +19,15 @@ var secondsOutput;
 var minutesOutput;
 var minutesDigits = document.getElementById('minutesDigits');
 var secondsDigits = document.getElementById('secondsDigits');
+
+var finalScore = "";
+var scoreSeconds = "";
+var scoreMinutes = "";
+
+var score = {
+    name: 
+
+}
 
 //opening screen full width div with button
 //button starts game and closes div
@@ -43,6 +61,34 @@ function counter(){
     minutesDigits.innerHTML = minutesOutput;
 }
 
+//function to add 15 seconds to timer and to change color of the timer digits
+function addSeconds(){
+    seconds = seconds + 15;
+    secondsDigits.style.color = "#F79128";
+    minutesDigits.style.color = "#F79128";
+    dots.style.color = "#F79128";
+    addFive.style.display= "inline";
+    //return styles to normal after 2 seconds
+    timeoutColor = setTimeout(function(){
+        secondsDigits.style.color = "#ffffff";
+        minutesDigits.style.color = "#ffffff";
+        dots.style.color = "#ffffff";
+        addFive.style.display="none";
+    }, 2000);
+}
+
+function endTimer(){
+    scoreMinutes = minutesOutput;
+    console.log(scoreMinutes);
+    scoreSeconds = secondsOutput;
+    console.log(scoreSeconds);
+    clearInterval(timer);
+}
+
+function saveScore(){
+    enterNameDiv.style.display="none";
+    scoreBoard.style.display="block";
+}
 //create carosel for questions and answers
 //function to change answer appearance once clicked
 
@@ -65,3 +111,20 @@ document.querySelectorAll('.answer').forEach(item => {
     })
   });
 // END code from flaviocopes.com
+
+document.querySelectorAll('.incorrect').forEach(item => {
+    item.addEventListener('click', event => {
+      addSeconds();
+    })
+  });
+
+//add events to all buttons with class of last  
+document.querySelectorAll('.last').forEach(item => {
+    item.addEventListener('click', event => {
+        endTimer();
+        navBar.style.display="none";
+        myCarousel.style.display="none";
+        enterNameDiv.style.display="flex";
+        yourScore.innerHTML = scoreMinutes + ":" + scoreSeconds;
+    })
+  });
