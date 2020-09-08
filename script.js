793b6a38ee8efdfc425bdc6c7823ff31295b9f6f
nick75mowbray@gmail.com <57086864+nick75mowbray@users.noreply.github.com>
2020-09-08 17:54:11 +0930

    add questions with carousel function

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
+var startBtn = document.getElementById('begin-btn');
+var openingDiv = document.getElementById('opener');
+var answerbtn = document.getElementsByClassName("answer");
+var navBar = document.getElementById('navbar');
+var myCarousel = document.getElementById('mycarousel');
+var timer;
 
 //opening screen full width div with button
 //button starts game and closes div
 
+startBtn.addEventListener("click", function(){
+    openingDiv.style.display="none";
+    navBar.style.display="block";
+    myCarousel.style.display="block";
+});
 //create timer to count up
 //create function that increases timer if incorrect answer
-
+function counter(){
+    
+}
+timer = setInterval(counter, 1000);
 //create carosel for questions and answers
 //function to change answer appearance once clicked
 
@@ -13,13 +27,11 @@
 //store name and score as an object
 
 //new page for high scores
-$('.carousel').carousel('pause');
-var answerbtn = document.getElementsByClassName("answer");
 
 
 function nextQuestion() {
     $('.carousel').carousel('next')
-}
+};
 
 //add functions to every button with class of answer
 // START code from https://flaviocopes.com/how-to-add-event-listener-multiple-elements-javascript/
